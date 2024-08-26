064416155d79b626c4bdf8497bae8839e0db414d
원민관 <mkwan5741@gmail.com>
2024-08-26 15:46:17 +0900

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>doq</div>;
+}
+
+export default App;
---
+++ frontend/src/layout/common/ComponentWrapper.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ComponentWrapper() {
+  return <div>컴포넌트 래퍼</div>;
+}
+
+export default ComponentWrapper;
