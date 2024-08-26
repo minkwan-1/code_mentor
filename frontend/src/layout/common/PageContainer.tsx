1e86f819a408572b5bd40e8368e4592cb6b0ba66
원민관 <mkwan5741@gmail.com>
2024-08-26 15:49:09 +0900

    PageContainer 생성

--- frontend/src/layout/common/PageContainer.tsx
+++ frontend/src/layout/common/PageContainer.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { ReactNode } from "react";
+import { Box, SxProps, Theme } from "@mui/material";
+import Appbar from "../appbar/Appbar";
+import Footer from "../footer/Footer";
 
-const PageContainer = () => {
-  return <div>페이지 컨테이너</div>;
+interface PageContainerProps {
+  children: ReactNode;
+  sx?: SxProps<Theme>;
+}
+
+const PageContainer = ({ children, sx }: PageContainerProps) => {
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        ...sx,
+      }}
+    >
+      <Appbar />
+      <Box sx={{ flexGrow: 1 }}>{children}</Box>
+      <Footer />
+    </Box>
+  );
 };
 
 export default PageContainer;
